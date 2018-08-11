55ff233816841ea1221435b72e5c5b81407f5f27
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-11 08:01:41 +0100

    adds selling price getter test

--- specs/book_spec.rb
+++ specs/book_spec.rb
@@ -10,27 +10,31 @@ class TestBook < MiniTest::Test
   end
 
   def test_can_get_title
-    assert_equal("The Burgess Boys", @book1. title)
+    assert_equal("The Burgess Boys", @book1.title)
   end
 
   def test_can_get_genre
-    assert_equal("Literary Fiction", @book1. genre)
+    assert_equal("Literary Fiction", @book1.genre)
   end
 
   def test_can_get_author
-    assert_equal("Elizabeth Strout", @book1. author)
+    assert_equal("Elizabeth Strout", @book1.author)
   end
 
   def test_can_get_description
-    assert_equal("With a rare combination of brilliant storytelling, exquisite prose, and remarkable insight into character, Elizabeth Strout has brought to life two deeply human protagonists whose struggles and triumphs will resonate with readers long after they turn the final page.", @book1. description)
+    assert_equal("With a rare combination of brilliant storytelling, exquisite prose, and remarkable insight into character, Elizabeth Strout has brought to life two deeply human protagonists whose struggles and triumphs will resonate with readers long after they turn the final page.", @book1.description)
   end
 
   def test_can_get_quantity
-    assert_equal(5, @book1. quantity)
+    assert_equal(5, @book1.quantity)
   end
 
   def test_can_get_cost_price
     assert_equal(3, @book1.cost_price)
+  end
+
+  def test_can_get_selling_price
+    assert_equal(10, @book1.selling_price)
   end
 
 
