176fafcfe0dbbdb76b9d98acbee775b48e3f3a5a
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-11 08:33:38 +0100

    adds bookshop.sql - database file

---
+++ db/bookshop.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE authors (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255)
+);
+
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  author INT REFERENCES authors(id) ON DELETE CASCADE,
+  description TEXT,
+  quantity INTEGER,
+  cost_price NUMERIC,
+  selling_price NUMERIC
+);
