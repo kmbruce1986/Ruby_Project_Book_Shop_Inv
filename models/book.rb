84610cd1b2cfdc4f8fa23e069deed443ce11696d
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-11 07:50:39 +0100

    adds title getter test

---
+++ models/book.rb
@@ -0,0 +1,18 @@
+class Book
+
+  attr_reader :id
+  attr_accessor :title, :genre, :author: :description, :quantity, :cost_price, :selling_price
+
+  def initialize(id, title, genre, author, description, quantity, cost_price, selling_price)
+    @id = id
+    @title = title
+    @genre = genre
+    @author = author
+    @description = description
+    @quantity = quantity
+    @cost_price = cost_price
+    @selling_price = selling_price
+
+  end
+
+end
