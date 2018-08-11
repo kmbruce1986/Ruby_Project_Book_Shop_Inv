69b3dc9eee02b54bf95a18cab8237baf5ad37f86
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-11 07:37:51 +0100

    adds author getters - tests pass

---
+++ models/author.rb
@@ -0,0 +1,14 @@
+class Author
+
+  attr_reader :id
+  attr_accessor :first_name, :last_name
+
+  def initialize(id, first_name, last_name)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+
+  end
+
+
+end
--- specs/author_spec.rb
+++ specs/author_spec.rb
@@ -6,7 +6,19 @@ require_relative('../models/author.rb')
 class TestAuthor < MiniTest::Test
 
   def setup
+    @author1 = Author.new(1, "Elizabeth", "Strout")
+  end
 
+  def test_can_get_id
+  assert_equal(1, @author1.id)
+  end
+
+  def test_can_get_first_name
+  assert_equal("Elizabeth", @author1.first_name)
+  end
+
+  def test_can_get_last_name
+  assert_equal("Strout", @author1.last_name)  
   end
 
 end
