200918a420055364e0e2002bed5cbd19e017013e
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-11 09:17:24 +0100

    adds save function to Book class and fixes author_id issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require('pry-byebug')
 
 require_relative('../models/author.rb')
+require_relative('../models/book.rb')
 
 Author.delete_all
 
@@ -11,6 +12,17 @@ author1 = Author.new({
 
 author1.save
 
+book1 = Book.new({
+  'title' => "The Burgess Boys",
+  'genre' => "Literary Fiction",
+  'author_id' => author1.id,
+  'description' => "With a rare combination of brilliant storytelling, exquisite prose, and remarkable insight into character, Elizabeth Strout has brought to life two deeply human protagonists whose struggles and triumphs will resonate with readers long after they turn the final page.",
+  'quantity' => 5,
+  'cost_price' => 3,
+  'selling_price' => 10
+  })
+
+book1.save
 
 
 
--- models/book.rb
+++ models/book.rb
@@ -7,7 +7,7 @@ class Book
     @id = options['id'].to_i if options['id']
     @title = options['title']
     @genre = options['genre']
-    @author_id = options['author']
+    @author_id = options['author_id']
     @description = options['description']
     @quantity = options['quantity'].to_i
     @cost_price = options['cost_price']
@@ -20,6 +20,12 @@ class Book
     return markup
   end
 
+  def save()
+    sql = "INSERT INTO books (title, genre, author_id, description, quantity, cost_price, selling_price) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id"
+    values = [@title, @genre, @author_id, @description, @quantity, @cost_price, @selling_price]
+    book = SqlRunner.run(sql, values).first
+    @id = book['id'].to_i
+  end
 
 
 end
