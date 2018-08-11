62182b4315eb31fc783ed1fc274feadb01414d34
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-11 08:56:45 +0100

    adds update function to Author

--- models/author.rb
+++ models/author.rb
@@ -34,4 +34,9 @@ class Author
     return author = Author.new(result)
   end
 
+  def update()
+    sql = "UPDATE authors SET (first_name, last_name) = ($1, $2) WHERE id = $3"
+    values = [@first_name, @last_name, @id]
+    SqlRunner.run(sql, values)
+  end
 end
