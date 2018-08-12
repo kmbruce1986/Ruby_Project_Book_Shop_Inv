cde12bc584a62287734435be23a29ed3fd2876e6
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-12 14:18:26 +0100

    adds basic book erb and moves controller file

---
+++ controller.rb
@@ -0,0 +1,11 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( 'models/author.rb' )
+require_relative( 'models/book.rb' )
+
+also_reload( 'models/*' )
+
+get '/books/' do
+  @books = Book.all
+  erb(:books)
+end
--- controllers/controller.rb
+++
@@ -1,6 +0,0 @@
-require( 'sinatra' )
-require( 'sinatra/contrib/all' )
-require_relative( '../models/author.rb' )
-require_relative( '../models/book.rb' )
-
-also_reload( 'models/*' )
